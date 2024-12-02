5377bf78d26c2c1be18682c98095340019e00603
24LP2767 24LP2767 <24LP2767@G301-CC0194.local>
2024-12-02 17:15:09 +0800

    variable

---
+++ index.js
@@ -0,0 +1,24 @@
+console.log("hello world from JS variavles")
+
+
+let age = 15;
+
+console.log(age);
+
+age = 40
+
+console.log(age);
+
+const name = "tuvshu";
+
+console.log(name);
+
+
+let MyName = 'tuvshee';
+
+console.log(MyName)
+
+c
+
+
+
